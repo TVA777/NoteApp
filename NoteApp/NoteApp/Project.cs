bfb8f4c674f6ff449b2a68c86dc2973670fbc9ee
Kostyn <sysadkrd@gmail.com>
2018-12-15 13:16:19 +0300

    Разработана логика приложения: Note, Project, ProjectManager

--- NoteApp/NoteApp/Project.cs
+++ NoteApp/NoteApp/Project.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace NoteApp
 {
-	public class Project
+    public class Project
 	{
 		public List<Note> Notes = new List<Note>();
 	}
